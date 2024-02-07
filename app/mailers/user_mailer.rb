f9fd3754f6c460a13c833a5687507940ffc7b6e7
yuler <is.yuler@gmail.com>
2024-02-07 16:46:01 +0800

    code style

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def welcome
     @user = params[:user]
 
-    mail to: @user.email, subject: 'Welcome to our site'
+    mail to: @user.email, subject: "Welcome to our site"
   end
 end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -4,8 +4,8 @@ class UserMailerTest < ActionMailer::TestCase
   test "welcome" do
     user = users(:one)
     mail = UserMailer.with(user: user).welcome
-    
-    assert_equal [user.email], mail.to
+
+    assert_equal [ user.email ], mail.to
     assert_match "Welcome", mail.subject
     assert_match "Hi", mail.body.encoded
   end
