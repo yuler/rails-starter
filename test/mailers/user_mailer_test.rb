577807f84681e96dfe3d31c2379a617e29abf96d
yuler <is.yuler@gmail.com>
2024-02-07 16:44:14 +0800

    Setup email

---
+++ test/mailers/user_mailer_test.rb
@@ -0,0 +1,12 @@
+require "test_helper"
+
+class UserMailerTest < ActionMailer::TestCase
+  test "welcome" do
+    user = users(:one)
+    mail = UserMailer.with(user: user).welcome
+    
+    assert_equal [user.email], mail.to
+    assert_match "Welcome", mail.subject
+    assert_match "Hi", mail.body.encoded
+  end
+end
