65fefeb00e48c4c2d3c5e96ecfc68ecc58769ccc
yuler <is.yuler@gmail.com>
2024-03-12 18:50:37 +0800

    WIP: Add user validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
 
   normalizes :email, with: ->(email) { email.strip.downcase }
 
