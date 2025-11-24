b6c3500da2f2909f759dfe22e2091e5d6faf7d14
yuler <is.yuler@gmail.com>
2025-11-24 17:40:31 +0800

    Fix fixtures & tests for CI

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,8 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_24_073534) do
   create_table "accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
-    t.string "name"
     t.integer "kind", default: 0, null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["name"], name: "index_accounts_on_name", unique: true
