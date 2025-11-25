a007349e5868cdc15b64a1eb55eed1447ae3de77
yuler <is.yuler@gmail.com>
2025-11-25 13:16:36 +0800

    Update configuration for solid components

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[8.1].define(version: 2025_11_24_073534) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "pg_catalog.plpgsql"
-
   create_table "accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "description"
     t.integer "kind", default: 0, null: false
     t.string "name"
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["name"], name: "index_accounts_on_name", unique: true
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
@@ -65,7 +62,7 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_24_073534) do
     t.string "ip_address"
     t.datetime "updated_at", null: false
     t.string "user_agent"
-    t.bigint "user_id", null: false
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
--- docs/postgres.md
+++ docs/postgres.md
@@ -3,4 +3,13 @@
 - Update the database driver in [Gemfile](../Gemfile) to `pg`
 - Modify [.env.example] to include PostgreSQL environment variables
 - Use [config/database.postgres.yml](../config/database.postgres.yml) to overwrite `database.yml`
+- Regenerate the database schema for PostgreSQL
+
+```bash
+rails solid_cache:install
+rails solid_queue:install
+rails solid_cable:install
+rails db:drop db:create db:migrate
+```
+
 - Use [compose.example.postgres.yml](../compose.example.postgres.yml) for Docker Compose deployment
