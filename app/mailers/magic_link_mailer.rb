0287907d5cea40d57ff8b202d3935318fe0d6e5f
yuler <is.yuler@gmail.com>
2025-12-31 09:37:53 +0800

    Fix typo

--- app/mailers/magic_link_mailer.rb
+++ app/mailers/magic_link_mailer.rb
@@ -1,7 +1,7 @@
 class MagicLinkMailer < ApplicationMailer
   def sign_in(magic_link)
     @magic_link = magic_link
-    @identity = @magic_link.identify
+    @identity = @magic_link.identity
 
     mail to: @identity.email, subject: "Sign in to your account"
   end
