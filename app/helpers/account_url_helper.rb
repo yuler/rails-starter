3ebea367b1a6be8db3b6cff6a3f52103c224de35
yuler <is.yuler@gmail.com>
2026-01-05 18:06:51 +0800

    Split header/footer layout partials

---
+++ app/helpers/account_url_helper.rb
@@ -0,0 +1,21 @@
+module AccountUrlHelper
+  def sign_in_url
+    main_app.new_session_path(script_name: nil)
+  end
+
+  def sign_out_url
+    main_app.new_session_path
+  end
+
+  def my_accounts_url
+    main_app.session_accounts_url(script_name: nil)
+  end
+
+  def redirect_to_sign_in_url
+    redirect_to sign_in_url, allow_other_host: true
+  end
+
+  def redirect_to_sign_out_url
+    redirect_to sign_out_url, allow_other_host: true
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
     account_name = if Current.account && Current.session&.identity&.users&.many?
       Current.account&.name
     end
-    tag.title [ @page_title, account_name, "Fizzy" ].compact.join(" | ")
+    # TODO: Rename
+    tag.title [ @page_title, account_name, "Rails Starter" ].compact.join(" | ")
   end
 end
