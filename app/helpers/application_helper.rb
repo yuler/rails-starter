2cd4303745fe221aa731b8467137486679267628
yuler <is.yuler@gmail.com>
2026-01-05 16:58:10 +0800

    Split _head.html.erb partial & add page_title_tag helper & @page_title variable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def page_title_tag
+    account_name = if Current.account && Current.session&.identity&.users&.many?
+      Current.account&.name
+    end
+    tag.title [ @page_title, account_name, "Fizzy" ].compact.join(" | ")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -30,8 +30,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
+  get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
   root "home#show"
